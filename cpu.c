996d0e258becdbc293dfbf5a9bf3d5023ba2d854
Matt Smith <mrs9107@g.rit.edu>
2016-01-24 16:52:54 -0500

    first commit

---
+++ cpu.c
@@ -0,0 +1,13 @@
+/*************************************************
+ * NES EMU-LATER opensource nes emulator
+ * Author: Matt Smith mrs9107@g.rit.edu
+ *
+ * purpose: to emulate the cpu portion of the nintendo entertainment system
+ *
+ * Date: 1-24-16
+ * *********************************************/
+
+#include <stdio.h>
+
+
+//fuck man idk what to write just yet
