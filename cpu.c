13ceab2cc3bfbc20b283b0a29cbd8ef7c385e8c6
Joe <jvd5839@g.rit.edu>
2016-04-02 20:35:31 -0400

    Committing

--- cpu.c
+++ cpu.c
@@ -1,7 +1,9 @@
 /*************************************************
  * NES EMU-LATER opensource nes emulator
+ * Product Owner: 
+ * Project Manager: Joseph Diment jvd5839@g.rit.edu
  * Author: Matt Smith mrs9107@g.rit.edu
- *
+ * 
  * purpose: to emulate the cpu portion of the nintendo entertainment system
  *
  * Date: 1-24-16
