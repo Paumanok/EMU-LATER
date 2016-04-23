395f716b052061559a38dfb0ffc87186f2aff4c5
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2016-04-23 16:12:12 -0400

    initializer for cpu and a reset function added

--- cpu.c
+++ cpu.c
@@ -10,6 +10,33 @@
  * *********************************************/
 
 #include <stdio.h>
+#include "cpu.h"
+
+int cpu(){
+
+    return 0;
+}
+
+//initalizes the memory for the cpu registers
+//
+int initCPU(MOS6502 *cpu ){
+    cpu->pc = malloc(sizeof(uint16_t));
+    cpu->A  = malloc(sizeof(uint8_t));
+    cpu->Y  = malloc(sizeof(uint8_t));
+    cpu->X  = malloc(sizeof(uint8_t));
+    cpu->S  = malloc(sizeof(uint8_t));
+
+   return 1; //should add malloc checks to avoid segfaults 
+}
+
+//resets the CPU's registers on boot
+//takes a pointer to the cpu struct
+void reset(MOS6502 *cpu){
+    cpu->pc = 0x0;
+    cpu->A  = 0x0;
+    cpu->Y  = 0x0;
+    cpu->X  = 0x0;
+    cpu->S  = 0x0;
+}
 
 
-//fuck man idk what to write just yet
