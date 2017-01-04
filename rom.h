45ac91d340323f31ce646538fa246c6e0cd3451b
Matthew Smith <guitarbass95@gmail.com>
2017-01-04 11:01:14 -0500

    small changes

--- rom.h
+++ rom.h
@@ -6,5 +6,15 @@
 
 
 struct romFile{
-
+    uint8_t NES[4];
+    uint8_t PRG_ROM_SIZE;
+    uint8_t CHR_ROM_SIZE;
+    uint8_t INST_ROM;
+    uint8_t PROM;
 }
+
+struct header{
+    uint8_t rom_size_offset;
+    uint8_t chr_size_offset;
+}
+
